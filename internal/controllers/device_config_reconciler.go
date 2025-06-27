44edc540188830e475f90eaafff737c1d191e4a6
Yevgeny Shnaidman <yshnaidm@redhat.com>
2025-06-27 16:24:00 +0300

    Adjusting GO version and modules' dependencies
    This commit includes the following
    1. moving to the GO version 1.24
    2. moving to KMM version 2.4 (the latest release)
    3. resolving go.mod dependencies
    4. mock generations for unit-test
    5. unit tests and lint fixes

--- internal/controllers/device_config_reconciler.go
+++ internal/controllers/device_config_reconciler.go
@@ -20,11 +20,11 @@ import (
 	"context"
 	"fmt"
 
-	kmmv1beta1 "github.com/rh-ecosystem-edge/kernel-module-management/api/v1beta1"
 	awslabsv1alpha1 "github.com/awslabs/operator-for-ai-chips-on-aws/api/v1alpha1"
 	"github.com/awslabs/operator-for-ai-chips-on-aws/internal/kmmmodule"
 	"github.com/awslabs/operator-for-ai-chips-on-aws/internal/nodelabeller"
 	"github.com/awslabs/operator-for-ai-chips-on-aws/internal/nodemetrics"
+	kmmv1beta1 "github.com/rh-ecosystem-edge/kernel-module-management/api/v1beta1"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
